4a14acce2403307c047ef80828a301be11798048
goalhunter <parthbhanderi16@gmail.com>
2025-01-02 18:54:54 -0600

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
+#!/bin/bash
 source venv/bin/activate
-
-python -m streamlit run streamlit_app.py --server.port 8000 --server.address 0.0.0.0
+python -m streamlit run streamlit_app.py --server.port $PORT --server.address 0.0.0.0
