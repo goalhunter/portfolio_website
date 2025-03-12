a1c3b23722990f4470c718f94be4814e06fe5d60
Parth Bhanderi <parthbhanderi16@gmail.com>
2025-03-12 16:17:21 -0500

    pinecone error fixed

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 source venv/bin/activate
 pip uninstall pinecone-client -y
 pip uninstall pinecone -y
+
 pip install pinecone
 pip install -r requirements.txt
 python -m streamlit run streamlit_app.py --browser.gatherUsageStats False --server.port 8000 --server.address 0.0.0.0
